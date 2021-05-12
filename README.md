3dd2f089484844ef9c787fbbfaf038242fabe414
Vinay Lingampally <vinay@cyive.com>
2021-05-12 10:48:11 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Nanoid-generator
+
+Utility extension to use 'nanoid' in visual studio code.
+
+## Usage
+
+In options menu
+
+- `ctrl+shift+p` for windows
+- `⇧⌘P` in macos
+
+searching for 'nanoid', gives two options
+
+1. Insert new nanoid
+1. Copy to clipboard
+
+## Settings
+
+### Length
+
+You can provide the length of the nanoid to be generated, It takes value from 2 - 36.
+Use this link to understand your requirement. [Nanoid collision calculator](https://zelark.github.io/nano-id-cc/)
+
+## Credits
+
+- [Nanoid - Original repo](https://github.com/ai/nanoid/)
+- [Nanoid collision detetctor](https://zelark.github.io/nano-id-cc/)
+
+## Developer
+
+contact at hello@cyglot.com
---
+++ extension.js
@@ -0,0 +1,54 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const { nanoid } = require("nanoid");
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "nanoid" is now active!');
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  const disposables = [];
+  disposables.push(
+    vscode.commands.registerCommand("nanoid-generator.generate", function () {
+      // The code you place here will be executed every time your command is executed
+
+      // Display a message box to the user
+      let config = vscode.workspace.getConfiguration("nanoid");
+      let nanoIdLength = config.get("length", 10);
+      vscode.env.clipboard.writeText(nanoid(nanoIdLength));
+      vscode.commands.executeCommand("editor.action.clipboardPasteAction");
+    })
+  );
+
+  disposables.push(
+    vscode.commands.registerCommand("nanoid-generator.clipboard", function () {
+      // The code you place here will be executed every time your command is executed
+
+      // Display a message box to the user
+      let config = vscode.workspace.getConfiguration("nanoid-generator");
+      let nanoIdLength = config.get("length", 10);
+      vscode.env.clipboard.writeText(nanoid(nanoIdLength));
+      vscode.window.showInformationMessage("'nanoid' copied to clipboard");
+    })
+  );
+
+  context.subscriptions.concat(disposables);
+}
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
